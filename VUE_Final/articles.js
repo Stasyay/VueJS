7948fbe0bc288e9642c6559378d97ef3ed60b898
Nastya <tibeikina@inbox.ru>
2024-04-30 15:09:44 +0500

    add article.js

---
+++ VUE_Final/articles.js
@@ -0,0 +1,58 @@
+// import Vue from 'vue';
+// import App from './App.vue';
+
+// const app = new Vue({
+//     el: '#root',
+//     render: (h) => h(App),
+// });
+
+const app = new Vue({
+    el: '#app',
+    data: {
+        articles: [
+            {
+                id: 0,
+                title: 'Let’s Get Solution For Building Construction Work',
+                image: './img/BlogImg1.png',
+                tag: 'Kitchen Design',
+                date: '26 December,2022'
+            },
+            {
+                id: 1,
+                title: 'Let’s Get Solution For Building Construction Work',
+                image: './img/BlogImg2.png',
+                tag: 'Living Design',
+                date: '22 December,2022'
+            },
+            {
+                id: 2,
+                title: 'Let’s Get Solution For Building Construction Work',
+                image: './img/BlogImg3.png',
+                tag: 'Kitchen Design',
+                date: '26 December,2022'
+            },
+            {
+                id: 3,
+                title: 'Let’s Get Solution For Building Construction Work',
+                image: './img/BlogImg4.png',
+                tag: 'Kitchen Design',
+                date: '26 December,2022'
+            },
+            {
+                id: 4,
+                title: 'Low Cost Latest Invented Interior Designing Ideas.',
+                image: './img/BlogImg5.png',
+                tag: 'Living Design',
+                date: '22 December,2022'
+            },
+            {
+                id: 5,
+                title: 'Best For Any Office & Business Interior Solution',
+                image: './img/BlogImg6.png',
+                tag: 'Interior Design',
+                date: '25 December,2022'
+            },
+
+        ]
+    }
+})
