598128c819734bbe278e2e1e11332331fd8215ac
Nastya <tibeikina@inbox.ru>
2024-05-03 17:06:53 +0500

    something

--- VUE_Final/articles.js
+++ VUE_Final/articles.js
@@ -1,14 +1,10 @@
-// import Vue from 'vue';
-// import App from './App.vue';
-
-// const app = new Vue({
-//     el: '#root',
-//     render: (h) => h(App),
-// });
-
 const app = new Vue({
     el: '#app',
     data: {
+        selectedTag: [],
+        buttons: [
+            'Kitchen', 'Bedroom', 'Building', 'Architecture', 'Kitchen Planning', 'Bedroom'
+        ],
         articles: [
             {
                 id: 0,
@@ -35,7 +31,7 @@ const app = new Vue({
                 id: 3,
                 title: 'Let’s Get Solution For Building Construction Work',
                 image: './img/BlogImg4.png',
-                tag: 'Kitchen Design',
+                tag: 'Kitchen Planning',
                 date: '26 December,2022'
             },
             {
@@ -54,5 +50,35 @@ const app = new Vue({
             },
 
         ]
+    },
+    methods: {
+
+        // Необходимо, чтобы на выборе Tags происходила сортировка статей по выбранному тегу. Тег можно выбрать только один.
+        selectTag(e) {
+
+
+            // if (this.selectedTag.includes(e.target.textContent)) {
+            //     this.selectedTag = this.selectedTag.filter(
+            //         (elem) => elem != e.target.textContent
+            //     );
+            //     console.log(`if ${this.selectedTag}`);
+            //     e.target.classList.toggle("article__tag-selected");
+            //     console.log(e);
+            // } else {
+            //     this.selectedTag.push(e.target.textContent);
+            //     e.target.classList.toggle("article__tag-selected");
+            //     e.preventDefault();
+            //     console.log(e.preventDefault());
+            // }
+
+
+
+        }
+    },
+    computed: {
+        filteredArticles() {
+            return this.articles.filter(article => article.tag.includes(this.selectedTag))
+        }
     }
 })
+
