30f39b4f936c75ad477bf8ed5ffaa1489db88dce
Nastya <tibeikina@inbox.ru>
2024-05-13 15:05:37 +0500

    add HW

---
+++ HW5/project/src/components/ProductDetails.vue
@@ -0,0 +1,34 @@
+<template>
+<div>
+    <p>Product: {{ product.name }}</p>
+    <p>Price: {{ formattedPrice }}</p>
+    <p v-if="product.available">Available</p>
+    <p v-else>Out of stock</p>
+</div>
+
+</template>
+
+<script>
+export default {
+  name: 'ProductDetails',
+
+  data() {
+    return {
+      product: 
+        {
+            id: 1,
+            name: 'Product 3',
+            price: 99.99,
+            available: true
+        }
+    
+    };
+  },
+
+  computed: {
+    formattedPrice() {
+       return '$' + this.product.price
+    }
+  }
+};
+</script>
